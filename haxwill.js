ce9410a727b2e467886c6277be1ff08a1bba555a
William Hu <william.hqs@gmail.com>
2015-10-13 18:28:52 +0800

    Change structure and add images

--- haxwill.js
+++ haxwill.js
@@ -2,40 +2,6 @@
 //无论你怎么定义函数，js解释器是将这个函数当做一个Function对象，而函数名即是指向该对象的引用的一个变量，在这个变量后面加上(一对小括号)，就表示运行这个函数。
 (function(){
 
-  var nodesArray = ["HAX",
-                 "Medical",               //医疗
-                 "Advanced Manufacturing",//高级制造业
-                 "IoT Enablement",        // 物联网
-                 "Robotics",              //机器人
-                 // --------first--------
-                 "Consumer", //消费
-                 // -------second---------
-                 "Smart home",  //智能家居
-                 "Pet tech",    //宠物科技
-                 "Sport tech",  //运动科技
-                 "Food & Bev",  //视频饮料
-                 "Beauty",      //优美
-                 "Gaming",      //游戏
-                 "Lifestyle",   //生活方式(保健)
-                 "Audio",       //影音
-                 "HCI",         //人机交互
-                 "Ed-tech",     //教育技术
-                 // -------third--------- 16
-                  "Preemadonna", 
-                  "Flair",
-                  "Motion Metrics",
-                  "Kokoon",
-                  "Ourobotics",
-                  "Arduboy",
-                  "Electroloom",
-                  "Picbuy",
-                  "Moai",
-                  "Bartesian",
-                  "Feetme",
-                  "Aria"
-                  // -------fourth--------- 27
-                 ];
-
 	//渲染画布
 	var Renderer = function(canvas){
 	    var dom    = $(canvas)
@@ -68,15 +34,19 @@
           that.initMouseHandling()
 
           particleSystem.eachNode(function(node, pt) {
-            if(node.data.image) {
-              node.data.imageob = new Image()
-              node.data.imageob.src = "images/" + node.data.image
+            var src = "images/"
+            var name = node.name.substring(3,node.name.length)
+            node.data.imageob = new Image()
+            if(!(node.name.includes("001"))) {
+              src += (name + ".png")
+              console.log(src)
             }
+            node.data.imageob.src = src//node.data.image
           })
       },
       
       redraw:function(){
-          ctx.fillStyle = "white"
+          ctx.fillStyle = "gray"
           
           // ctx.fillRect(0,0, 300, 300)
           ctx.fillRect(0,0, canvas.width, canvas.height)
@@ -92,11 +62,11 @@
 
   		    particleSystem.eachNode(function(node, pt){
             var imageob = node.data.imageob
-            var radius  = node.data.radius
             var imageW  = 50
             var imageH  = 50
             var w = Math.max(20, 20+gfx.textWidth(node.name) ) //这个是取文字的大小，但不能小于20
 
+            var name = node.name.substring(3,node.name.length) //去除数字
             var ww = Math.max(60,imageW)
             if (node.data.alpha===0) return
             if (node.data.shape=='dot'){
@@ -105,20 +75,19 @@
                   ctx.drawImage(imageob, pt.x-imageW/2, pt.y-imageH/2-7, imageW, imageH)  
                   // gfx.text(node.name, pt.x, (pt.y+imageH/2+6), {color:"white", align:"center", font:"Arial", size:24})
               }else{ 
-                if(hasWhiteSpace(node.name)) {
-                  var words = node.name.split(' ')
+                if(hasWhiteSpace(name)) {
+                  var words = name.split(' ')
                   for(var i=0; i<words.length; i++) {
                     gfx.text(words[i], pt.x, pt.y-8+13*(i+1), {color:"white", align:"center", font:"Arial", size:24})
                   }
                 }else{
-                  gfx.text(node.name, pt.x, pt.y+7, {color:"white", align:"center", font:"Arial", size:24})
-                  // gfx.text("nihao", pt.x, pt.y+7+15, {color:"white", align:"center", font:"Arial", size:12})
+                  gfx.text(name, pt.x, pt.y+7, {color:"white", align:"center", font:"Arial", size:24})
                 }
               }
               // gfx.text(node.name, pt.x, pt.y+7, {color:"white", align:"center", font:"Arial", size:12})
             }else{
               gfx.rect(pt.x-w/2, pt.y-8, w, 20, 4, {fill:node.data.color, alpha:node.data.alpha})
-              gfx.text(node.name||"", pt.x, pt.y+9, {color:"white", align:"center", font:"Arial", size:24})
+              gfx.text(name||"", pt.x, pt.y+9, {color:"white", align:"center", font:"Arial", size:24})
               // gfx.text(node.name||"", pt.x, pt.y+9, {color:"green", align:"center", font:"Arial", size:12})
             }
           })
@@ -249,14 +218,9 @@
 
 	//Javascript 只有在DOM元素已经定义以后才可以对其执行某种操作，jQuery使用document.ready来保证所要执行的代码是在DOM元素被加载完成的情况下执行
 	$(document).ready(function(){
-    
 		  var sys = arbor.ParticleSystem(512,1000,1.0)
 		  sys.parameters({gravity:true})
     	sys.renderer = Renderer("#viewcanvas")
-      
-      // $("#viewcanvas").width = 800
-      // $("#viewcanvas").height = 600
-      
     	//examples
     	// sys.addEdge('a','b')
     	// sys.addNode('HAX',{color:"red", shape:"dot", alpha:1,name:"haha"})
@@ -270,102 +234,11 @@
         company:"green"
 	    }
       
+      var data = $.getJSON("json/nodes.json",function(data){
+          sys.graft({nodes:data.nodes, edges:data.edges})
+      })
 
-		  var theUI = {
-		      nodes:{[nodesArray[0]]:{color:"", shape:"dot",level:"first", alpha:1,link:'',image: 'haxsticker.png',radius:'30'},
-		             [nodesArray[1]]:{color:CLR.demo, shape:"dot",level:"first",alpha:1,image:'2.pic.jpg', link:'http://www.hax.co'},
-		             [nodesArray[2]]:{color:CLR.demo, shape:"dot",level:"first", alpha:1,link:'http://www.hax.co'},
-		             [nodesArray[3]]:{color:CLR.demo,shape:"dot",level:"first", alpha:1,link:'http://www.hax.co'},
-		             [nodesArray[4]]:{color:CLR.demo,shape:"dot",level:"first", alpha:1,link:'http://www.hax.co'},
-
-		             [nodesArray[5]]:{color:CLR.demo,shape:"dot",level:"second", alpha:1,link:'http://www.hax.co'},
-
-                 [nodesArray[6]]:{color:CLR.doc,shape:"dot",level:"third", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[7]]:{color:CLR.doc,shape:"dot",level:"third", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[8]]:{color:CLR.doc,shape:"dot",level:"third", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[9]]:{color:CLR.doc,shape:"dot",level:"third", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[10]]:{color:CLR.doc,shape:"dot",level:"third", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[11]]:{color:CLR.doc,shape:"dot",level:"third", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[12]]:{color:CLR.doc,shape:"dot",level:"third", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[13]]:{color:CLR.doc,shape:"dot",level:"third", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[14]]:{color:CLR.doc,shape:"dot",level:"third", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[15]]:{color:CLR.doc,shape:"dot",level:"third", alpha:0, link:'#'+[nodesArray[6]]},
-
-                 [nodesArray[16]]:{color:CLR.company,shape:"dot",level:"fourth", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[17]]:{color:CLR.company,shape:"dot",level:"fourth", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[18]]:{color:CLR.company,shape:"dot",level:"fourth", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[19]]:{color:CLR.company,shape:"dot",level:"fourth", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[20]]:{color:CLR.company,shape:"dot",level:"fourth", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[21]]:{color:CLR.company,shape:"dot",level:"fourth", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[22]]:{color:CLR.company,shape:"dot",level:"fourth", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[23]]:{color:CLR.company,shape:"dot",level:"fourth", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[24]]:{color:CLR.company,shape:"dot",level:"fourth", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[25]]:{color:CLR.company,shape:"dot",level:"fourth", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[26]]:{color:CLR.company,shape:"dot",level:"fourth", alpha:0, link:'#'+[nodesArray[6]]},
-                 [nodesArray[27]]:{color:CLR.company,shape:"dot",level:"fourth", alpha:0, link:'#'+[nodesArray[6]]}
-
-		             // Robotics:{color:CLR.doc, alpha:0, link:'http://www.revols.com'},
-             		 // Flair:{color:CLR.doc, alpha:0, link:'http://www.sosv.com'}
-		             // echolalia:{color:CLR.demo, alpha:0, link:'/echolalia'},
-
-		             // docs:{color:CLR.branch, shape:"dot", alpha:1}, 
-		             // reference:{color:CLR.doc, alpha:0, link:'#reference'},
-		             // introduction:{color:CLR.doc, alpha:0, link:'#introduction'},
-
-		             // code:{color:CLR.branch, shape:"dot", alpha:1},
-		             // github:{color:CLR.code, alpha:0, link:'https://github.com/samizdatco/arbor'},
-		             // ".zip":{color:CLR.code, alpha:0, link:'/js/dist/arbor-v0.92.zip'},
-		             // ".tar.gz":{color:CLR.code, alpha:0, link:'/js/dist/arbor-v0.92.tar.gz'}
-		            },
-		      edges:{
-		        [nodesArray[0]]:{
-		          [nodesArray[1]]:{length:3, pointSize:3},
-		          [nodesArray[2]]:{length:8},
-		          [nodesArray[3]]:{length:6},
-		          [nodesArray[4]]:{length:7},
-              [nodesArray[5]]:{length:2}
-		        },
-            [nodesArray[5]]:{
-              [nodesArray[6]]:{length:5},
-              [nodesArray[7]]:{length:6},
-              [nodesArray[8]]:{length:7},
-              [nodesArray[9]]:{length:8},
-              [nodesArray[10]]:{length:9},
-              [nodesArray[11]]:{length:10},
-              [nodesArray[12]]:{length:11},
-              [nodesArray[13]]:{length:12},
-              [nodesArray[14]]:{length:13},
-              [nodesArray[15]]:{length:5}
-            },
-            [nodesArray[6]]:{
-              [nodesArray[16]]:{length:6},
-              [nodesArray[17]]:{length:6}
-            },
-            [nodesArray[7]]:{
-              [nodesArray[18]]:{length:6},
-              [nodesArray[19]]:{length:6}
-            },
-            [nodesArray[8]]:{
-              [nodesArray[20]]:{length:6},
-              [nodesArray[21]]:{length:6}
-            },
-            [nodesArray[9]]:{
-              [nodesArray[22]]:{length:6},
-              [nodesArray[23]]:{length:6}
-            },
-            [nodesArray[10]]:{
-              [nodesArray[23]]:{length:6},
-              [nodesArray[24]]:{length:6},
-              [nodesArray[25]]:{length:6},
-              [nodesArray[26]]:{length:6},
-              [nodesArray[27]]:{length:6}
-            },
-		      }
-		    }
-    	sys.graft(theUI)
     })
 
 
 })()
-
-
